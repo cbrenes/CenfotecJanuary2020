388aea4d19119a7543210178b2e7ff196f575bfa
Cesar <cesarbresola@gmail.com>
2020-03-06 18:52:09 -0600

    added class #2

---
+++ Clase#2/Clase#2/NamesListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NamesListViewController.swift
+//  Clase#2
+//
+//  Created by Cesar Brenes on 2/29/20.
+//  Copyright © 2020 VeuxLabs. All rights reserved.
+//
+
+import UIKit
+
+class NamesListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+}
+
+extension NamesListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "")!
+    }
+    
+}
